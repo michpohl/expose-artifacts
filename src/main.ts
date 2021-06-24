571cf121ef87686312b691576cf670f24f1a9a00
Michael Pohl <contact@michaelpohl.de>
2021-06-24 13:43:46 +0200

    Build first experimental version

--- src/main.ts
+++ src/main.ts
@@ -1,47 +1,55 @@
 import * as core from '@actions/core'
 import * as github from '@actions/github'
 
-async function exposeArtifacts(): Promise<void> {
-  const inputs = {
-    token: core.getInput('github-token', {required: true})
-  };
+const inputs = {
+  token: core.getInput('github-token', { required: true })
+}
 
-  const client = new github.GitHub(inputs.token);
+
+async function exposeArtifacts(): Promise<void> {
+  const octokit = github.getOctokit(inputs.token)
   const run_id = github.context.payload.workflow_run.id
-  const listWorkflowsArtifacts = await client.actions.listWorkflowRunArtifacts({
+  const listWorkflowsArtifacts = await octokit.rest.actions.listWorkflowRunArtifacts(
+    {
       owner: github.context.repo.owner,
       repo: github.context.repo.repo,
       run_id: run_id
-  });
-
+    }
+  )
+  const artifacts = listWorkflowsArtifacts.data.artifacts
   const suite_id = github.context.payload.workflow_run.check_suite_id
   const owner = github.context.repo.owner
   const repo = github.context.repo.repo
-  const artifacts = listWorkflowsArtifacts.data.artifacts.map((i : any) => {
-    console.log
+  for (const a of artifacts) {
+    console.log(a)
+
+  }
+  artifacts.map(i => {
+    console.log(i)
     return {
       id: i.id,
       name: i.name,
       suite_id: suite_id,
-      url_download: 'https://github.com/'+owner+'/'+repo+'/suites/'+suite_id+'/artifacts/'+i.id
+      url_download: 'https://github.com/' + owner + '/' + repo + '/suites/' + suite_id + '/artifacts/' + i.id,
     }
   })
-
-  console.log('::group::List outputs variables')
-  // Generate outputs
-  artifacts.forEach((a) => {
-    console.log('::set-output name='+a.name+'::'+a.url_download)
-    console.log('set-output name='+a.name+'::'+a.url_download)
-  })
-  console.log('::endgroup::')
 }
+
+// console.log('::group::List outputs variables')
+// // Generate outputs
+// artifacts.forEach((a) => {
+//   console.log('::set-output name=' + a.name + '::' + a.url_download)
+//   console.log('set-output name=' + a.name + '::' + a.url_download)
+// })
+// console.log('::endgroup::')
+
 
 async function run(): Promise<void> {
-  try {
-    await exposeArtifacts();
-  } catch (error) {
-    core.setFailed(error.message)
-  }
-}
+      try {
+        await exposeArtifacts();
+      } catch (error) {
+        core.setFailed(error.message)
+      }
+    }
 
 run()
